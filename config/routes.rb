08ec65d56c3bc1db11ca3dc7bdf8ac233f6e524f
jirgens <jessa.irg@gmail.com>
2014-05-06 13:42:36 -0700

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get '/logout', to: 'session#new'
   
   resources :posts, except: [:destroy] do
 		resources :comments, only: [:create]
