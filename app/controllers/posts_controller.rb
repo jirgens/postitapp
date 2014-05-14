7125e6676d86ebfbf8c0c3de6dc7b6f375f4c6d2
jirgens <jessa.irg@gmail.com>
2014-05-14 11:05:28 -0700

    voting enabled on posts and comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :vote]
   before_action :require_user, except: [:show, :index]
 
   def index
@@ -35,6 +35,17 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def vote
+    @vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
+      if @vote.valid?
+        flash[:notice]= "Your vote was counted."
+        
+      else
+        flash[:error] = "You can only vote once per post."
+      end
+      redirect_to :back
   end
 
   private
