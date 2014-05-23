db01643edbd8fbac938049907f707536c6f0e330
jirgens <jessa.irg@gmail.com>
2014-05-23 12:56:14 -0700

    voteable module added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+	include Voteable
+
 	belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
 	has_many :comments
 	has_many :post_categories
@@ -11,17 +13,7 @@ class Post < ActiveRecord::Base
 
 	after_validation :generate_slug
 
-	def total_votes
-		up_votes - down_votes
-	end
-
-	def up_votes
-		self.votes.where(vote: true).size
-	end
-
-	def down_votes
-		self.votes.where(vote: false).size
-	end
+	
 
 	def to_param
 		self.slug
---
+++ lib/voteable.rb
@@ -0,0 +1,48 @@
+# using concerns
+module Voteable
+	extend ActiveSupport::Concern 
+
+	included do 
+		has_many :votes, as: :voteable
+	end
+
+	def total_votes
+		self.up_votes - self.down_votes
+	end
+
+	def up_votes
+		self.votes.where(vote: true).size
+	end
+
+	def down_votes
+		self.votes.where(vote: false).size
+	end
+end
+
+
+# module Voteable
+# 	def self.included(base)
+# 		base.send(:include, InstanceMethods)
+# 		base.extend ClassMethods
+# 	end
+
+# 	module InstanceMethods
+# 		def total_votes
+# 			up_votes - down_votes
+# 		end
+
+# 		def up_votes
+# 			self.votes.where(vote: true).size
+# 		end
+
+# 		def down_votes
+# 			self.votes.where(vote: false).size
+# 		end
+# 	end
+
+# 	module ClassMethods
+# 		def my_class_method
+# 			has_many :votes, as: :voteable
+# 		end
+# 	end
+# end
