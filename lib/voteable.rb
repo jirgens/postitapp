b474a9863716d18eb79f1062b738834143b42881
jirgens <jessa.irg@gmail.com>
2014-05-23 15:37:12 -0700

    update

--- lib/voteable.rb
+++ lib/voteable.rb
@@ -1,23 +1,23 @@
-# using concerns
-module Voteable
-	extend ActiveSupport::Concern 
+# # using concerns
+# module Voteable
+# 	extend ActiveSupport::Concern 
 
-	included do 
-		has_many :votes, as: :voteable
-	end
+# 	included do 
+# 		has_many :votes, as: :voteable
+# 	end
 
-	def total_votes
-		self.up_votes - self.down_votes
-	end
+# 	def total_votes
+# 		self.up_votes - self.down_votes
+# 	end
 
-	def up_votes
-		self.votes.where(vote: true).size
-	end
+# 	def up_votes
+# 		self.votes.where(vote: true).size
+# 	end
 
-	def down_votes
-		self.votes.where(vote: false).size
-	end
-end
+# 	def down_votes
+# 		self.votes.where(vote: false).size
+# 	end
+# end
 
 
 # module Voteable
