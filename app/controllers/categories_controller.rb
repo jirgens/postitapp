229377ebb06032543319b07dc5b2516a1d4d175d
jirgens <jessa.irg@gmail.com>
2014-08-08 17:11:02 -0700

    category badges

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,17 +3,12 @@ class CategoriesController < ApplicationController
 	before_action :require_admin, only: [:new, :create]
 	before_action :require_user, only: [:new, :create]
 
-	# def index
-	# 	@categories = Category.all
-	# end
-
 	def show
 		@category = Category.find_by slug: params[:id]
 	end
 
 	def new
 		@category = Category.new
-		# require_user(:new, 'create a category')
 	end
 
 	def create
@@ -27,27 +22,10 @@ class CategoriesController < ApplicationController
 		end
 	end
 
-	# def edit
-	# 	require_user(:edit, 'edit this category')
-	# end
-
-	# def update
-
-	# 	if @category.update(category_params)
-	# 		flash[:notice] = 'Your category was updated'
-	# 		redirect_to category_path(@category)
-	# 	else
-	# 		render :edit
-	# 	end
-	#end
-
 	private
 
-		def category_params
-			params.require(:category).permit(:name)
-		end
-
-		# def set_category
-		# 	@category = Category.find_by slug: params[:id]
-		# end
+	def category_params
+		params.require(:category).permit(:name)
 	end
+
+end
